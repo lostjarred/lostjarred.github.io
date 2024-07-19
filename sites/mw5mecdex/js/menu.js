7821750995034540004eacbc2dd858b21685c27a
lostjarred <58093147+lostjarred@users.noreply.github.com>
2024-07-20 09:05:36 +1000

    update mecadex website

--- sites/mw5mecdex/js/menu.js
+++ sites/mw5mecdex/js/menu.js
@@ -16,13 +16,15 @@
     menutext += '<ul class="menu">';
     menutext += '<li> <a href="./hm_archer_whyowepaistthbl.html" class="button">\ARCHER-ARC-2P-WHILE YOUR WERE PARTYING I STUDIED THE BLADE</a> </li>';
     menutext += '<li> <a href="./hm_jagermech_pompom.html" class="button">\JAGERMECH-JM6-S-POM POM</a> </li>';
-    menutext += '<li> <a href="./hm_marauder_splat.html" class="button">\MARAUDER-MAD-3M-SPLAT</a> </li>';
+    menutext += '<li> <a href="./hm_marauder_splatv1.html" class="button">\MARAUDER-MAD-3M-SPLAT V1</a> </li>';
+    menutext += '<li> <a href="./hm_marauder_splatv2.html" class="button">\MARAUDER-MAD-3M-SPLAT V2</a> </li>';
     menutext += '</ul>';
     menutext += '<h3>ASSUALT MECHS</h3>';
     menutext += '<ul class="menu">';
     menutext += '<li> <a href="./am_kingcrab_mg.html" class="button">\KINGCRAB-KGC-000-UNKOWN MG</a> </li>';
     menutext += '<li> <a href="./am_stalker_eye.html" class="button">\STALKER-STK-4N-EYE</a> </li>';
     menutext += '<li> <a href="./am_stalker_sauron.html" class="button">\STALKER-STK-4N-SAURON</a> </li>';
+    menutext += '<li> <a href="./am_atlas_noescape.html" class="button">\ATLAS-AS-P-NOESCAPE</a> </li>';
     menutext += '</ul>';
     menudiv.innerHTML = menutext;
 }
