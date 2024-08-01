1457cb01d8df8d25a658dd56c085af610476c801
Jarred <58093147+lostjarred@users.noreply.github.com>
2024-08-02 09:40:44 +1000

    update mecha dex website

--- sites/mw5mecdex/js/menu.js
+++ sites/mw5mecdex/js/menu.js
@@ -8,10 +8,14 @@
     menutext += '</ul>';
     menutext += '<h3>LIGHT MECHS</h3>';
     menutext += '<ul class="menu">';
-    menutext += '<li> <a href="./lm_raven_runngun.html" class="button">\RVN-1X-RunNGun</a> </li>';
-    menutext += '<li> <a href="./lm_urban_scootandshoot.html" class="button">\UM-SA1-ScootandShoot</a> </li>';
+    menutext += '<li> <a href="./lm_raven_runngun.html" class="button">\RVN-1X-RUNNGUN</a> </li>';
+    menutext += '<li> <a href="./lm_urban_scootandshoot.html" class="button">\UM-SA1-SCOOTANDSHOOT</a> </li>';
     menutext += '</ul>';
     menutext += '<h3>MEDIUM MECHS</h3>';
+    menutext += '<ul class="menu">';
+    menutext += '<li> <a href="./mm_hunchback_lancer.html" class="button">\HBK-VEST-LANCER</a> </li>';
+    menutext += '<li> <a href="./mm_hunchback_lancer_v2.html" class="button">\HBK-VEST-LANCER-V2</a> </li>';
+    menutext += '</ul>';
     menutext += '<h3>HEAVY MECHS</h3>';
     menutext += '<ul class="menu">';
     menutext += '<li> <a href="./hm_archer_whyowepaistthbl.html" class="button">\ARCHER-ARC-2P-WHILE YOUR WERE PARTYING I STUDIED THE BLADE</a> </li>';
@@ -22,9 +26,15 @@
     menutext += '<h3>ASSUALT MECHS</h3>';
     menutext += '<ul class="menu">';
     menutext += '<li> <a href="./am_kingcrab_mg.html" class="button">\KINGCRAB-KGC-000-UNKOWN MG</a> </li>';
+    menutext += '<li> <a href="./am_kingcrab_v2.html" class="button">\KINGCRAB-KGC-000-UNKOWN V2</a> </li>';
     menutext += '<li> <a href="./am_stalker_eye.html" class="button">\STALKER-STK-4N-EYE</a> </li>';
     menutext += '<li> <a href="./am_stalker_sauron.html" class="button">\STALKER-STK-4N-SAURON</a> </li>';
-    menutext += '<li> <a href="./am_atlas_noescape.html" class="button">\ATLAS-AS-P-NOESCAPE</a> </li>';
+    menutext += '<li> <p>ATLAS-AS7-P-NOESCAPE</p> </li>';
+    menutext += '<ul class="menu" style="margin-left: 2%;">';
+    menutext += '<li> <a href="./am_atlas_noescape.html" class="button">\ATLAS-AS7-P-NOESCAPE V1</a> </li>';
+    menutext += '<li> <a href="./am_atlas_noescapev2.html" class="button">\ATLAS-AS7-P-NOESCAPE V2</a> </li>';
+    menutext += '<li> <a href="./am_atlas_noescapev3.html" class="button">\ATLAS-AS7-P-NOESCAPE V3</a> </li>';
+    menutext += '</ul>';
     menutext += '</ul>';
     menudiv.innerHTML = menutext;
 }
